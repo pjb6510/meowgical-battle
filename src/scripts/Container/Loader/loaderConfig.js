c5378b35384c75082ec1e388499bbad8227471b4
ParkJeongbeen <pjb6510@gmail.com>
2021-05-08 20:15:40 +0900

    Update: add player image in loaderConfig

--- src/scripts/Container/Loader/loaderConfig.js
+++ src/scripts/Container/Loader/loaderConfig.js
@@ -1,9 +1,14 @@
 import mainBackground from "../../../assets/images/main/main.png";
 import battleBackground from "../../../assets/images/battleBackground/battleBackground.png";
+import leftPlayer from "../../../assets/images/L_jump/1.png";
+import rightPlayer from "../../../assets/images/R_jump/1.png";
 
 const loaderConfig = {
   mainBackground,
   battleBackground,
+
+  leftPlayer,
+  rightPlayer,
 };
 
 export default loaderConfig;
