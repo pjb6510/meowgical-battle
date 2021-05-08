07e6b7d72119747bc5be482db0648f53924bf3ce
ParkJeongbeen <pjb6510@gmail.com>
2021-05-08 20:16:05 +0900

    Update: add borderProperty in createBox

--- src/scripts/pixiUtils/createBox.js
+++ src/scripts/pixiUtils/createBox.js
@@ -1,17 +1,32 @@
 import * as PIXI from "pixi.js";
 import { DropShadowFilter } from "@pixi/filter-drop-shadow";
 
-const createBox = ({ width, height, x, y, color }) => {
-  const box = new PIXI.Graphics();
-  box.beginFill(color);
-  box.drawRoundedRect(
-    x,
-    y,
+const createBox = (boxParams) => {
+  const {
     width,
     height,
-    20
-  );
-  box.endFill();
+    x,
+    y,
+    color,
+    borderWidth = 0,
+    borderColor = 0x000000,
+    borderAlpha = 1,
+    borderAlignment = 1,
+  } = boxParams;
+
+  const box = new PIXI.Graphics();
+
+  box
+    .lineStyle(borderWidth, borderColor, borderAlpha, borderAlignment)
+    .beginFill(color)
+    .drawRoundedRect(
+      x,
+      y,
+      width,
+      height,
+      20
+    )
+    .endFill();
 
   box.pivot.set(
     width / 2,
