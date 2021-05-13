8d69daae4c06d748ece1db9c4e0e8c48ec0a38ef
ParkJeongbeen <pjb6510@gmail.com>
2021-05-14 00:33:50 +0900

    Update: add hit portrait in loaderConfig

--- src/assets/loaderConfig.js
+++ src/assets/loaderConfig.js
@@ -3,7 +3,9 @@ import battleBackground from "./images/battleBackground/battleBackground.png";
 import leftPlayer from "./images/L_jump/1.png";
 import rightPlayer from "./images/R_jump/1.png";
 import leftPlayerPortrait from "./images/L_portrait/leftPlayerPortrait.png";
+import leftPlayerHitPortrait from "./images/L_portrait/leftPlayerHitPortrait.png";
 import rightPlayerPortrait from "./images/R_portrait/rightPlayerPortrait.png";
+import rightPlayerHitPortrait from "./images/R_portrait/rightPlayerHitPortrait.png";
 import upArrow from "./images/arrows/upArrow.png";
 import downArrow from "./images/arrows/downArrow.png";
 import leftArrow from "./images/arrows/leftArrow.png";
@@ -17,7 +19,9 @@ const loaderConfig = {
   rightPlayer,
 
   leftPlayerPortrait,
+  leftPlayerHitPortrait,
   rightPlayerPortrait,
+  rightPlayerHitPortrait,
 
   upArrow,
   downArrow,
