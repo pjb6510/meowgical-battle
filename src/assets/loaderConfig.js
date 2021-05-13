1beaecb36ce1ac5bb647743d14fd4ad24a56c74c
ParkJeongbeen <pjb6510@gmail.com>
2021-05-13 22:31:18 +0900

    Add: charcter portrait image

--- src/assets/loaderConfig.js
+++ src/assets/loaderConfig.js
@@ -2,6 +2,8 @@ import mainBackground from "./images/main/main.png";
 import battleBackground from "./images/battleBackground/battleBackground.png";
 import leftPlayer from "./images/L_jump/1.png";
 import rightPlayer from "./images/R_jump/1.png";
+import leftPlayerPortrait from "./images/portrait/leftPlayerPortrait.png";
+import rightPlayerPortrait from "./images/portrait/rightPlayerPortrait.png";
 import upArrow from "./images/arrows/upArrow.png";
 import downArrow from "./images/arrows/downArrow.png";
 import leftArrow from "./images/arrows/leftArrow.png";
@@ -13,6 +15,9 @@ const loaderConfig = {
 
   leftPlayer,
   rightPlayer,
+
+  leftPlayerPortrait,
+  rightPlayerPortrait,
 
   upArrow,
   downArrow,
