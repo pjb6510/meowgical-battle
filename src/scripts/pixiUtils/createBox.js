b147480c2cb401f4f796f982615a722fdf61455a
ParkJeongbeen <pjb6510@gmail.com>
2021-05-14 12:14:12 +0900

    Refactor: createBox param

--- src/scripts/pixiUtils/createBox.js
+++ src/scripts/pixiUtils/createBox.js
@@ -1,27 +1,30 @@
 import * as PIXI from "pixi.js";
 import { DropShadowFilter } from "@pixi/filter-drop-shadow";
 
-const createBox = (boxOption) => {
-  const {
-    width,
-    height,
-    x,
-    y,
-    color,
-    radius = 20,
-    borderWidth = 0,
-    borderColor = 0x000000,
-    borderAlpha = 1,
-    borderAlignment = 1,
-    pivotX = width / 2,
-    pivotY = height / 2,
-    hasShadow = true,
-  } = boxOption;
-
+const createBox = ({
+  width,
+  height,
+  x,
+  y,
+  color,
+  radius = 20,
+  borderWidth = 0,
+  borderColor = 0x000000,
+  borderAlpha = 1,
+  borderAlignment = 1,
+  pivotX = width / 2,
+  pivotY = height / 2,
+  hasShadow = true,
+}) => {
   const box = new PIXI.Graphics();
 
   box
-    .lineStyle(borderWidth, borderColor, borderAlpha, borderAlignment)
+    .lineStyle(
+      borderWidth,
+      borderColor,
+      borderAlpha,
+      borderAlignment
+    )
     .beginFill(color)
     .drawRoundedRect(
       x,
