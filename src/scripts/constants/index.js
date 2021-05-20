7104746e3fb8e944798903a1685e7bb7b7be94d8
ParkJeongbeen <pjb6510@gmail.com>
2021-05-20 20:20:22 +0900

    Update: add actions in game

--- src/scripts/constants/index.js
+++ src/scripts/constants/index.js
@@ -1,5 +1,16 @@
-export const broadcastedActions = {
+export const actionsInRoom = {
   ENTER: "ENTER",
   START_GAME: "START_GAME",
   SEND_PEER: "SEND_PEER",
+};
+
+export const actionsInGame = {
+  MOVE_FRONT: "MOVE_FRONT",
+  MOVE_BACK: "MOVE_BACK",
+  MOVE_UP: "MOVE_UP",
+  MOVE_DOWN: "MOVE_DOWN",
+  BE_HIT: "BE_HIT",
+  CAST_FIREBALL: "CAST_FIREBALL",
+  CAST_LIGHTNING: "CAST_LIGHTNING",
+  CAST_MINE: "CAST_MINE",
 };
