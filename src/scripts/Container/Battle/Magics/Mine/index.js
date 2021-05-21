f711893fa764d297ce45f92897914f4ee7f21d47
ParkJeongbeen <pjb6510@gmail.com>
2021-05-21 15:49:33 +0900

    Update: flip opponent arrowIcon x direction

--- src/scripts/Container/Battle/Magics/Mine/index.js
+++ src/scripts/Container/Battle/Magics/Mine/index.js
@@ -41,7 +41,7 @@ export default class Mine extends Magic {
     this.createSprite();
     this.createExplosionSprite();
 
-    this.durationTime = 8000;
+    this.durationTime = 10000;
 
     this.handleHit = this.explode;
 
