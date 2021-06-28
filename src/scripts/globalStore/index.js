24e067829efbd5ae2b3ede75074a984e4bc6f24a
ParkJeongbeen <pjb6510@gmail.com>
2021-06-28 18:37:27 +0900

    Update: globalStore to singleton pattern

--- src/scripts/globalStore/index.js
+++ src/scripts/globalStore/index.js
@@ -1,5 +1,37 @@
-import GlobalStore from "./GlobalStore";
+const globalStore = (() => {
+  const _store = {};
+  const _subscribers = [];
 
-const globalStore = new GlobalStore();
+  return {
+    setStore(key, value) {
+      _store[key] = value;
+      this.handleStoreChange();
+    },
+
+    getItem(key) {
+      return _store[key];
+    },
+
+    handleStoreChange() {
+      for (let i = 0; i < _subscribers.length; i += 1) {
+        const subscribeCallback = _subscribers[i];
+        subscribeCallback(_store);
+      }
+    },
+
+    subscribe(callback) {
+      _subscribers.push(callback);
+    },
+
+    unsubscribe(callback) {
+      const subscriberIndex = _subscribers
+        .findIndex((subscriber) => callback === subscriber);
+
+      if (subscriberIndex !== -1) {
+        _subscribers.splice(subscriberIndex, 1);
+      }
+    },
+  };
+})();
 
 export default globalStore;
